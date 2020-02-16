a31f22c4755dff8caa4c298c7ac32f66671723e6
Thea Leake <thea.leake@gmail.com>
2020-02-16 10:37:47 -0800

    Added tab and backtick, shifted vol to third level.

--- keyboards/atreus/keymaps/thea/keymap.c
+++ keyboards/atreus/keymaps/thea/keymap.c
@@ -22,27 +22,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /*
    *  1       2        3         4        5             ||          6        7        8       9        0
-   *  -       +        (         )        '             ||          left     down     up      right    :
-   *  volup   voldown  [         ]        `             ||          #        ~        @       *         \
+   *  tab     `        (         )        '             ||          left     down     up      right    :
+   *  -       +        [         ]        =             ||          #        ~        @       *         \
    *  ctrl    gui      alt       shift   bksp   L3      || L3       enter    shift    alt     gui      ctrl
    */
   [_RS] = LAYOUT( /* [> RAISE <] */
     KC_1,    KC_2,     KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,   KC_9,     KC_0,
-    KC_MINS, KC_PLUS,  KC_LPRN, KC_RPRN, KC_QUOT,                   KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT, KC_COLN,
-    KC_VOLD, KC_VOLU,  KC_LBRC, KC_RBRC, KC_GRV,                    KC_HASH, KC_TILD, KC_AT,  KC_ASTR,  KC_BSLS ,
+    KC_TAB,  KC_GRV,   KC_LPRN, KC_RPRN, KC_QUOT,                   KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT, KC_COLN,
+    KC_MINS, KC_PLUS,  KC_LBRC, KC_RBRC, KC_EQL,                    KC_HASH, KC_TILD, KC_AT,  KC_ASTR,  KC_BSLS ,
     KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_BSPC, MO(_LW), MO(_LW), KC_ENT,  KC_TRNS, KC_TRNS,KC_TRNS,  KC_TRNS
   ),
 
   /*
-   * F1      F2        F3       F4       F5             ||          F6       F7       F8       F9       F10
-   * __      __        __       __       __             ||          home     pgdown   pgup     end      __
-   * __      __        __       __       __             ||          __       __       __       __       __
-   *  ctrl    gui      alt       CAPS   __     NA      || NA       __       CAPS    alt     gui      ctrl
+   * F1      F2        F3       F4       F5             ||          F6       F7       F8      F9        F10
+   * volup   voldown   __       __       __             ||          home     pgdown   pgup    end       __
+   * __      __        __       __       RESET          ||          __       __       __      __        __
+   *  ctrl    gui      alt      CAPS     __     NA      || NA       __       CAPS     alt     gui       ctrl
    */
   [_LW] = LAYOUT( /* [> LOWER <] */
-    KC_F1,  KC_F2, KC_F3,   KC_F4,  KC_F5,                   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10  ,
-    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                   KC_HOME, KC_PGDN,   KC_PGUP,   KC_END,   KC_NO  ,
-    KC_NO,   KC_VOLU, KC_NO,   KC_NO,   KC_NO,                     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO  ,
-    KC_TRNS, KC_TRNS,  KC_TRNS, KC_CAPS, KC_NO, KC_TRNS, KC_TRNS, KC_NO,  KC_CAPS, KC_TRNS,KC_TRNS,  KC_TRNS
+    KC_F1,   KC_F2,    KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,  KC_F9,    KC_F10,
+    KC_VOLD, KC_VOLU,  KC_NO,   KC_NO,   KC_NO,                     KC_HOME, KC_PGDN, KC_PGUP,KC_END,   KC_NO,
+    KC_NO,   KC_VOLU,  KC_NO,   KC_NO,   RESET,                     KC_NO,   KC_NO,   KC_NO,  KC_NO,    KC_NO,
+    KC_TRNS, KC_TRNS,  KC_TRNS, KC_CAPS, KC_NO, KC_TRNS, KC_TRNS,   KC_NO,   KC_CAPS, KC_TRNS,KC_TRNS,  KC_TRNS
     )
 };
